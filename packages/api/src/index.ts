95742d33e19840edda0d10f5c029356527907536
piyoppi <garakuta.toolbox@gmail.com>
2025-05-14 08:18:20 +0900

    20250514080000

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,22 +1,28 @@
 import { Hono } from 'hono'
 import { createKeywordHandler } from './handlers/getStationSummaries'
-import { OpenAPIV3_1 } from 'openapi-types'
 import oas from '@piyoppi/sansaku-api-spec/openapi.json'
 import { paths } from '@piyoppi/sansaku-api-spec/openapi.d.ts'
 import { cors } from 'hono/cors'
-import { queryValidator } from './validators/queryValidator'
+import { buildQueryValidator } from './validators/queryValidator'
+import { OpenAPIV3_1 } from 'openapi-types'
+
+type Paths = paths & {
+  [key: string]: never
+}
 
 export const createApp = (
   app: Hono,
   databaseFileName: string
 ) => {
+  const queryValidator = buildQueryValidator<Paths>(oas as OpenAPIV3_1.Document)
+
   const getStationSummariesFromKeywordHandler = createKeywordHandler(databaseFileName)
 
   app.use('*', cors())
 
   app.get(
     '/stations',
-    queryValidator<paths['/stations']['get']['parameters']['query']>(oas.paths['/stations'].get.parameters as OpenAPIV3_1.ParameterObject[]),
+    queryValidator('/stations', 'get'),
     (c) => {
       const params = c.req.valid('query')
 
--- packages/api/src/validators/queryValidator.ts
+++ packages/api/src/validators/queryValidator.ts
@@ -3,21 +3,36 @@ import Ajv from 'ajv'
 import type { Env, MiddlewareHandler } from 'hono'
 import { OpenAPIV3_1 } from 'openapi-types'
 
-export const queryValidator = <
-  T extends Record<string, string>
+type SchemaType = Record<
+  string, 
+  {
+    [key in 'get' | 'post' | 'put']?: {
+      parameters: {
+        query?: Record<string, unknown>
+      }
+    }
+  }
+>
+
+export const buildQueryValidator = <
+  InputOpenAPIParams extends SchemaType
 >(
-  schema: OpenAPIV3_1.ParameterObject[]
-): MiddlewareHandler<
+  schema: OpenAPIV3_1.Document
+) => <
+  Path extends Extract<keyof InputOpenAPIParams, string>,
+  Method extends keyof InputOpenAPIParams[Path] & ('get' | 'post' | 'put'),
+  QueryParams = InputOpenAPIParams[Path][Method] extends { parameters: { query: infer Q } } ? Q : never
+>(path: Path, method: Method): MiddlewareHandler<
   Env,
   string,
   {
-    in: { query: T }
-    out: { query: T }
+    in: { query: QueryParams }
+    out: { query: QueryParams }
   }
 > => {
   const ajv = new Ajv()
 
-  const targetSchemas = schema.filter((item) => item.in === 'query')
+  const targetSchemas = schema.paths?.[path as string]?.[method]?.parameters?.filter((item) => ('in' in item) && item.in === 'query') as OpenAPIV3_1.ParameterObject[]
   
   const properties = targetSchemas
     .reduce((acc, item) => {
@@ -46,5 +61,5 @@ export const queryValidator = <
     }
 
     return value
-  })
+  }) as MiddlewareHandler<Env, string, { in: { query: QueryParams }; out: { query: QueryParams } }>
 }
